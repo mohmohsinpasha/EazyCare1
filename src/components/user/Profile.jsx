ebbbde2e5bce91187c55d7d704bc4ad17390afed
MOHAMMED MUBBASHIR AHMED <97329674+MubbashirAhmed123@users.noreply.github.com>
2025-01-27 13:58:27 +0530

    some changes made in profile and userinfo page

--- src/components/user/Profile.jsx
+++ src/components/user/Profile.jsx
@@ -83,58 +83,7 @@ export default function Profile() {
             </div>
 
         {/* Profile Information */}
-        <div className="mt-6 lg:mt-0 lg:flex-1">
-          <h2 className="bg-gray-50 text-3xl font-medium text-neutral-800">{userData.name}</h2>
-          <hr className="bg-zinc-400 h-[1px] border-none mt-2" />
-
-          {/* Contact Information */}
-          <h3 className="text-neutral-500 underline mt-4">CONTACT INFORMATION</h3>
-          <table className="w-full text-left mt-2">
-            <tbody>
-              <tr>
-                <th className="pr-4">Email Id:</th>
-                <td>{userData.email }</td>
-              </tr>
-              <tr>
-                <th className="pr-4">Phone:</th>
-                <td>{userData.phone}</td>
-              </tr>
-              <tr>
-                <th className="pr-4">Address: </th>
-                <td>{userData?.address ? userData.address : 'no data'}</td>
-              </tr>
-            </tbody>
-          </table>
-
-          {/* Basic Information */}
-          <h3 className="text-neutral-500 underline mt-4">BASIC INFORMATION</h3>
-          <table className="w-full text-left mt-2">
-            <tbody>
-              <tr>
-                <th className="pr-4">Gender:</th>
-                <td>{userData?.gender ? userData.gender : 'no data'}</td>
-              </tr>
-              <tr>
-                <th className="pr-4">DOB:</th>
-                <td>{userData?.dob ? userData.dob : 'no data'}</td>
-              </tr>
-            </tbody>
-          </table>
-
-          {/* Action Buttons */}
-          <div className="mt-6 flex gap-4">
-
-
-
-            <button onClick={handleEdit} className="border border-primary px-6 py-2 rounded-full hover:bg-primary hover:text-gray-900 hover:bg-gray-400">
-              Edit
-            </button>
-            <button className="border border-primary px-6 py-2 rounded-full hover:bg-primary hover:text-gray-900 hover:bg-gray-400">
-
-              Save Information
-            </button>
-          </div>
-          </div>
+   
         </div>
       </div>
     </div>
